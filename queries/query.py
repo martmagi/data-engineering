622f50284c2c3b7dec18e82a11d51472cac72507
Mart Mägi <mart.magi@guardtime.com>
2022-01-02 21:00:06 +0200

    Fix strange whitespace issue that happened when editing via GitHub

--- queries/query.py
+++ queries/query.py
@@ -1,8 +1,14 @@
 import pymongo
-​
-client = pymongo.MongoClient('mongodb+srv://data-engineering-g12:<password>@data-engineering-g12.wvade.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+MONGO_USER = 'data-engineering-g12'
+MONGO_SECRET = 'mkj2xap*CJB1btn2gcz'
+MONGO_URL = 'data-engineering-g12.wvade.mongodb.net'
+MONGO_SCHEMA = 'myFirstDatabase'
+MONGO_CONNECTION_STRING = f'mongodb+srv://{MONGO_USER}:{MONGO_SECRET}@{MONGO_URL}/{MONGO_SCHEMA}?retryWrites=true&w=majority'
+
+client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
 db = client.memesDB
-​
+
 #Show the number of the memes per year in descending order (SQL)
 memes_per_year_pipeline = [
     {
@@ -23,19 +29,19 @@ memes_per_year_pipeline = [
     }
 ]
 memes_per_year = db.memes.aggregate(memes_per_year_pipeline)
-​
+
 print('Number of memes per year in descending order:')
 for doc in memes_per_year:
     print(doc)
 print('')
-​
+
 memes_containing_tag_finland = db.memes.find({"tags": "finland"})
-​
+
 print('Memes containing tag \'finland\':')
 for doc in memes_containing_tag_finland:
     print(doc)
 print('')
-​
+
 # Which memes have the largest number of common tags?
 common_tags_pipeline = [
     {
@@ -59,12 +65,12 @@ common_tags_pipeline = [
     }
 ]
 number_of_common_tags = db.memes.aggregate(common_tags_pipeline)
-​
+
 print('Memes that have the largest number of common tags:')
 for doc in number_of_common_tags:
     print(doc)
 print('')
-​
+
 # Rank memes by origin (youtube, 4chan, fb etc) (content>origin>links)
 memes_by_origin_pipeline = [
     {
@@ -88,12 +94,12 @@ memes_by_origin_pipeline = [
     }
 ]
 memes_by_origin = db.memes.aggregate(memes_by_origin_pipeline)
-​
+
 print('Rank memes by origin:')
 for doc in memes_by_origin:
     print(doc)
 print('')
-​
+
 # Something to Query about semantic relations of memes
 
 #Show the number of the memes per weekday in descending order (SQL)
