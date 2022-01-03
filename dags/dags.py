f0672dc9c421ae377e9581e791392acd64d9078b
Mart Mägi <mart.magi@guardtime.com>
2022-01-03 13:55:13 +0200

    Download raw and vision data in parallel branches & clean up some code

--- dags/dags.py
+++ dags/dags.py
@@ -1,8 +1,10 @@
 import datetime
+import os
 
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
+from airflow.utils.trigger_rule import TriggerRule
 
 from augmentation import augment
 from download_datasets import get_raw_dataset, get_vision_dataset
@@ -11,26 +13,21 @@ from transformation import transform
 from cleaning import clean
 
 DAGS_FOLDER = '/opt/airflow/dags/'
-KYM_FILE_PATH = DAGS_FOLDER + 'kym.json'
-CLEAN_FILE_PATH = DAGS_FOLDER + 'kym_clean.json'
-VISION_FILE_PATH = DAGS_FOLDER + 'kym_vision.json'
-AUGMENTED_FILE_PATH = DAGS_FOLDER + 'kym_augment.json'
-TRANSFORMED_FILE_PATH = DAGS_FOLDER + 'kym_transform.json'
+KYM_FILE_PATH = os.path.join(DAGS_FOLDER, 'kym.json')
+VISION_FILE_PATH = os.path.join(DAGS_FOLDER, 'kym_vision.json')
+CLEAN_FILE_PATH = os.path.join(DAGS_FOLDER, 'kym_clean.json')
+AUGMENTED_FILE_PATH = os.path.join(DAGS_FOLDER, 'kym_augment.json')
+TRANSFORMED_FILE_PATH = os.path.join(DAGS_FOLDER, 'kym_transform.json')
 
 default_args_dict = {
-    'start_date': datetime.datetime(2021, 11, 1, 0, 0, 0),
-    'concurrency': 1,
-    'schedule_interval': "35 12 * * 5",
-    'retries': 1,
-    'retry_delay': datetime.timedelta(minutes=5),
+    'start_date': datetime.datetime(2022, 1, 1, 0, 0, 0),
+    'schedule_interval': None
 }
 
 default_dag = DAG(
     dag_id='Data_Engineering_Project_G12',
     default_args=default_args_dict,
-    catchup=False,
-    template_searchpath=DAGS_FOLDER,
-    schedule_interval="35 12 * * 5",
+    template_searchpath=DAGS_FOLDER
 )
 
 get_raw_dataset_task = PythonOperator(
@@ -65,6 +62,7 @@ augmentation_task = PythonOperator(
     task_id='augment',
     dag=default_dag,
     python_callable=augment,
+    trigger_rule=TriggerRule.ALL_SUCCESS,
     op_kwargs={
         "clean_file_path": CLEAN_FILE_PATH,
         "vision_file_path": VISION_FILE_PATH,
@@ -96,6 +94,6 @@ neo4j_task = DummyOperator(
     dag=default_dag
 )
 
-# Run the tasks
-get_raw_dataset_task >> cleaning_task >> get_vision_dataset_task >> \
-augmentation_task >> transformation_task >> mongo_upload_task >> neo4j_task
+# Run the tasks. Two separate branches start with pulling two different datasets, and merging at augmentation task
+get_raw_dataset_task >> cleaning_task >> augmentation_task >> transformation_task >> mongo_upload_task >> neo4j_task
+get_vision_dataset_task >> augmentation_task >> transformation_task >> mongo_upload_task >> neo4j_task
--- dags/download_datasets.py
+++ dags/download_datasets.py
@@ -9,15 +9,17 @@ VISION_URL = 'https://owncloud.ut.ee/owncloud/index.php/s/teoFdWKBzzqcFjY/downlo
 
 def get_raw_dataset(kym_file_path: str):
     raw_file = Path(f"{kym_file_path}")
-    if not raw_file.is_file():  # skip downloading if it already exists
-        memes_raw_data = requests.get(RAW_DATA_URL).json()
-        with open(f"{kym_file_path}", 'w') as f:
-            json.dump(memes_raw_data, f, ensure_ascii=False)
+    if raw_file.is_file():  # skip downloading if it already exists
+        return
+    memes_raw_data = requests.get(RAW_DATA_URL).json()
+    with open(f"{kym_file_path}", 'w') as f:
+        json.dump(memes_raw_data, f, ensure_ascii=False)
 
 
 def get_vision_dataset(vision_file_path: str):
     vision_file = Path(f"{vision_file_path}")
-    if not vision_file.is_file():  # skip downloading if it already exists
-        vision_data = requests.get(VISION_URL).json()
-        with open(f"{vision_file_path}", 'w') as f:
-            json.dump(vision_data, f, ensure_ascii=False)
+    if vision_file.is_file():  # skip downloading if it already exists
+        return
+    vision_data = requests.get(VISION_URL).json()
+    with open(f"{vision_file_path}", 'w') as f:
+        json.dump(vision_data, f, ensure_ascii=False)
